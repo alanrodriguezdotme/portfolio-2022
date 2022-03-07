e7925cc54cb225d921541c8e923c63028643d086
Alan Rodriguez <info@alanrodriguez.me>
2022-03-07 09:11:13 -0800

    Adding voice-patterns demo

---
+++ public/demos/voice-patterns/precache-manifest.71d8135336b27cbe6729407f3475e45e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354463d18e3934f9c60d51c2cbe39111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8ae897309c57e4afa04",
+    "url": "/static/js/2.d4796117.chunk.js"
+  },
+  {
+    "revision": "f3fe3e8e00d8b8a66bdd728e1bacb3e3",
+    "url": "/static/js/2.d4796117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5db5e4ec1f859f572ca",
+    "url": "/static/js/main.05303cc8.chunk.js"
+  },
+  {
+    "revision": "62b9a5535eae9f83ca6b",
+    "url": "/static/js/runtime-main.4a7e645b.js"
+  }
+]);
