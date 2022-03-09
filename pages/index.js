335a4fa36ec878c70dc6f75154c901bd6569297c
Alan Rodriguez <info@alanrodriguez.me>
2022-03-09 08:08:11 -0800

    Added GA tag

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Layout from "../components/Layout";
 import ProjectCards from "../components/ProjectCards";
 import styles from "../styles/css/Home.module.css";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -10,6 +11,20 @@ export default function Home() {
       <Layout>
         <ProjectCards />
       </Layout>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-118957712-3"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analtyics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-118957712-3');`}
+      </Script>
     </div>
   );
 }
